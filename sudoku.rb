ff8223396ef51df39a0f4a5e2907ee77b1127161
Khushkaran Singh Bajwa <khushkaran@me.com>
2014-03-19 15:19:59 +0000

    Fixed readonly if statement

--- sudoku.rb
+++ sudoku.rb
@@ -66,6 +66,7 @@ get '/' do
   prepare_to_check_solution
   generate_new_puzzle_if_necessary
   @current_solution = session[:current_solution] || session[:puzzle]
+  @current_solution = session[:puzzle] if params[:reset]
   @solution = session[:solution]
   @puzzle = session[:puzzle]
   erb :index
@@ -74,7 +75,7 @@ end
 post '/' do
   cells = box_order_to_row_order(params["cell"])
   session[:current_solution] = cells.map{|value| value.to_i}.join
-  session[:check_solution] = true if !params[:save]
+  session[:check_solution] = true if !params[:save] && !params[:reset]
   redirect to("/")
 end
 
