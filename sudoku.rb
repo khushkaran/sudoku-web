85f96ddfc2f9331afcd9b3b2b50b20ac2c09f327
Khushkaran Singh Bajwa <khushkaran@me.com>
2014-03-19 13:56:45 +0000

    Extracted helpers and added layout file

--- sudoku.rb
+++ sudoku.rb
@@ -4,6 +4,7 @@ require 'rack-flash'
 require 'yaml'
 require_relative './lib/sudoku'
 require_relative './lib/cell'
+require_relative './helpers/application.rb'
 
 enable :sessions
 set :partial_template_engine, :erb
@@ -90,25 +91,8 @@ post '/set' do
   redirect to("/")
 end
 
-helpers do
-  def colour_class(solution_to_check, puzzle_value, current_solution_value, solution_value)
-    must_be_guessed = puzzle_value.to_i == 0
-    tried_to_guess = current_solution_value.to_i != 0
-    guessed_incorrectly = current_solution_value != solution_value
 
-    if solution_to_check && must_be_guessed && tried_to_guess && guessed_incorrectly
-      'incorrect'
-    elsif !must_be_guessed
-      'value-provided'
-    elsif !guessed_incorrectly
-      'correct'
-    end
-  end
 
-  def cell_value(value)
-    value.to_i == 0 ? '' : value
-  end
 
-end
 
 
